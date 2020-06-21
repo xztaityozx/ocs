e37cfa392f9dc5637d2e6e1aa8b0006b6351388b
xztaityozx <taityonohibi@gmail.com>
2020-06-21 21:19:10 +0900

    はい

---
+++ ocs/GlobalVariable.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ocs {
+    public class GlobalVariable {
+        public Dictionary<string, string> Env { get; }
+        private List<string> f;
+        private string f0;
+        public Regex Separator = new Regex(@"\s");
+
+        public string F0 {
+            get => f0;
+            set {
+                f0 = value;
+                f = null;
+                NR++;
+            }
+        }
+
+        public List<string> F => f ??= new List<string>{F0}
+            .Concat(
+                Separator.Split(F0).Where(s => !string.IsNullOrEmpty(s))
+            ).ToList();
+
+        /// <summary>
+        /// Number of fields
+        /// </summary>
+        public int NF => F.Count;
+
+        /// <summary>
+        /// Current line number
+        /// </summary>
+        public int NR = 0;
+
+        #region exports functions
+
+        public void print<T>(IEnumerable<T> l) {
+            foreach (var item in l) {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void print(object obj) => Console.WriteLine(obj);
+
+        public int i(string s) => int.Parse(s);
+
+        public decimal m(string s) => decimal.Parse(s, NumberStyles.Float);
+
+        public double d(string s) => double.Parse(s, NumberStyles.Float);
+
+        #endregion
+
+        /// <summary>
+        /// Set Environments
+        /// </summary>
+        /// <param name="key">key of environment</param>
+        /// <param name="value">value of environment</param>
+        public void SetEnv(string key, string value) {
+            if (Env.ContainsKey(key)) Env[key] = value;
+            else Env.Add(key, value);
+        }
+
+        public GlobalVariable(GlobalVariableOptions options = null) {
+            Env = new Dictionary<string, string>();
+
+            options ??= new GlobalVariableOptions();
+
+            // set global environment
+            if (!options.LoadGlobalEnvironments) return;
+
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables()) {
+                SetEnv($"{de.Key}", $"{de.Value}");
+            }
+        }
+    }
+
+    public class GlobalVariableOptions {
+        public bool LoadGlobalEnvironments { get; set; } = false;
+    }
+}
---
+++ ocs/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CommandLine;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace ocs {
+    internal class Program {
+        private static async Task Main(string[] args) {
+            var opt = Parser.Default.ParseArguments<Options>(args).MapResult(o => o, e => 
+                throw new AggregateException(e.Select(s=> new Exception(s.ToString())))
+            );
+            var gv = new GlobalVariable(
+                new GlobalVariableOptions {
+                    LoadGlobalEnvironments = opt.LoadEnvironments
+                }
+            );
+
+            if (!string.IsNullOrEmpty(opt.FieldSeparator)) gv.Separator = new Regex(opt.FieldSeparator);
+
+            // load assembles
+            var scriptOpt = ScriptOptions.Default.AddImports("System");
+            if (!string.IsNullOrEmpty(opt.Imports)) scriptOpt.AddImports(opt.Imports.Split(','));
+            
+            // BEGIN
+            var script = await CSharpScript.RunAsync(
+                opt.BeginBlock+";",
+                globalsType: typeof(GlobalVariable),
+                globals: gv,
+                options: scriptOpt
+            );
+
+            // CODE
+            using (var sr = new StreamReader(Console.OpenStandardInput())) {
+                while (sr.Peek() > 0) {
+                    gv.F0 = sr.ReadLine();
+                    await script.ContinueWithAsync(opt.Code);
+                }
+            }
+
+            // END
+            await script.ContinueWithAsync(opt.EndBlock);
+        }
+    }
+
+    public class Options {
+        [Option('b', "begin", Default = "", HelpText = "begin codes. this code runs at begin")]
+        public string BeginBlock { get; set; }
+        [Option('e', "end", Default = "", HelpText = "end codes. this code runs at after all")]
+        public string EndBlock { get; set; }
+        [Option('M', "imports", HelpText = "using Assembles")]
+        public string Imports { get; set; }
+        [Option('F', "field", Default = "", HelpText = "Field separator")]
+        public string FieldSeparator { get; set; }
+        [Option('f', "file", HelpText = "target file")]
+        public string File { get; set; }
+        [Option("env", HelpText = "load global environments")]
+        public bool LoadEnvironments { get; set; }
+        [Value(0, MetaName = "code", HelpText = "CODE block")]
+        public string Code { get; set; }
+    }
+}

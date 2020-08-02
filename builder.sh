09e74347107968ef436cd431b691dccef302657d
xztaityozx <taityonohibi@gmail.com>
2020-08-02 15:37:16 +0900

    add builder script

---
+++ builder.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+runtime_id="linux-x64"
+[[ $"{1}" = "macOS-latest" ]] runtime_id="osx-x64"
+
+
+cd ./ocs
+dotnet publish -r ${runtime_id} -c Release --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true -o ${runtime_id}

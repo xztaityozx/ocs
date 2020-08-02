506963415c9984df0ac62bdce74d1347fd6fe1bc
xztaityozx <taityonohibi@gmail.com>
2020-08-02 19:29:59 +0900

    いらねえ

--- builder.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-runtime_id="linux-x64"
-[[ "${1}" = "macOS-latest" ]] && runtime_id="osx-x64"
-
-
-cd ./ocs
-dotnet publish -r ${runtime_id} -c Release --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:Version="${2}" -o ${1}

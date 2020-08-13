ecfc77b43a50027b0a83e7f10f1b11c39f16b536
xztaityozx <taityonohibi@gmail.com>
2020-08-13 23:39:42 +0900

    LogError()を追加

--- ocs/Logger.cs
+++ ocs/Logger.cs
@@ -9,6 +9,7 @@ namespace ocs {
             Debug,
             Information,
             Warning,
+            Error,
             Critical
         }
 
@@ -21,12 +22,14 @@ namespace ocs {
             Console.WriteLine(obj);
         }
 
-        private static readonly Dictionary<LogLevel, Action<object>> DelegateTable = new Dictionary<LogLevel, Action<object>> {
-            [LogLevel.Debug] = o => Print(LogLevel.Debug,ConsoleColor.Green, o),
-            [LogLevel.Information] = o => Print(LogLevel.Information,ConsoleColor.Cyan, o),
-            [LogLevel.Warning] = o => Print(LogLevel.Warning,ConsoleColor.Yellow, o),
-            [LogLevel.Critical] = o => Print(LogLevel.Critical,ConsoleColor.Red, o)
-        };
+        private static readonly Dictionary<LogLevel, Action<object>> DelegateTable =
+            new Dictionary<LogLevel, Action<object>> {
+                [LogLevel.Debug] = o => Print(LogLevel.Debug, ConsoleColor.Green, o),
+                [LogLevel.Information] = o => Print(LogLevel.Information, ConsoleColor.Cyan, o),
+                [LogLevel.Warning] = o => Print(LogLevel.Warning, ConsoleColor.Yellow, o),
+                [LogLevel.Error] = o => Print(LogLevel.Critical, ConsoleColor.Red, o),
+                [LogLevel.Critical] = o => Print(LogLevel.Critical, ConsoleColor.DarkRed, o)
+            };
 
         public static void Log(LogLevel logLevel, object obj) {
             DelegateTable[logLevel](obj);
@@ -37,6 +40,8 @@ namespace ocs {
         public static void LogInformation(object obj) => DelegateTable[LogLevel.Information](obj);
 
         public static void LogWarning(object obj) => DelegateTable[LogLevel.Warning](obj);
+
+        public static void LogError(object obj) => DelegateTable[LogLevel.Error](obj);
 
         public static void LogCritical(object obj) => DelegateTable[LogLevel.Critical](obj);
     }

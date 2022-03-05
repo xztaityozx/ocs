db92dde5e9e4e3270ea2291d218f4110ca65836f
xztaityozx <taityonohibi@gmail.com>
2022-03-05 15:45:40 +0900

    デフォルト値が空配列になってへんかった

--- ocs/Lib/Config/Config.cs
+++ ocs/Lib/Config/Config.cs
@@ -2,22 +2,21 @@
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.Extensions.Logging;
 
-namespace ocs.Lib.Config
+namespace ocs.Lib.Config;
+
+public record Config
 {
-    public record Config
+    public LogLevel LogLevel { get; init; } = LogLevel.Error;
+    public string[] InlineCode { get; init; } = Array.Empty<string>();
+    public string[] ReferenceList { get; init; } = Array.Empty<string>();
+    public string[] UsingList { get; init; } = Array.Empty<string>();
+    public LanguageVersion LanguageVersion { get; init; } = LanguageVersion.CSharp10;
+
+    // ReSharper disable once EmptyConstructor
+    public Config() { }
+
+    public override string ToString()
     {
-        public LogLevel LogLevel { get; init; } = LogLevel.Error;
-        public string[] InlineCode { get; init; }
-        public string[] ReferenceList { get; init; }
-        public string[] UsingList { get; init; }
-        public LanguageVersion LanguageVersion { get; init; } = LanguageVersion.CSharp10;
-        // ReSharper disable once EmptyConstructor
-        public Config() { }
-
-        public override string ToString()
-        {
-            return JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
-        }
+        return JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
     }
-
 }
